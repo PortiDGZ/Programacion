b55bd3b8a33f29a23e0ae79241ddb9ea8cfb6ddf
PortiDGZ <55698790+PortiDGZ@users.noreply.github.com>
2020-01-10 20:50:53 +0100

    Seguimos con arrays

--- Ejercicios de Porti/src/src/ArrayBi1.java
+++ Ejercicios de Porti/src/src/ArrayBi1.java
@@ -2,7 +2,19 @@ public class ArrayBi1 {
 
     public static void main(String[] args){
 
-        
+        int[][] num = new int[3][6];
+
+        num[0][0] = 0;
+        num[1][1] = 30;
+        num[2][2] = 2;
+        num[5][5] = 5;
+        num[1][0] = 75;
+        num[1][4] = 0;
+        num[2][2] = -2;
+        num[2][3] = 9;
+        num[2][5] = 11;
+
+
 
     }
 }
---
+++ Ejercicios de Porti/src/src/ArrayBi2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ArrayBi2 {
+
+    public static void main(String[] args){
+
+    int[][] paco;
+
+    paco = generarArrayBi();
+
+    
+
+    }
+
+    public static int[][] generarArrayBi(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("¿De que tamaño quieres el array?: ");
+
+        int filas = entrada.nextInt();
+
+        int columnas = entrada.nextInt();
+
+        return new int[filas][columnas];
+
+    }
+
+    public static void rellenarmatriz(int[][] jose){
+
+        Scanner entrada = new Scanner(System.in);
+        for (int i = 0; i <= jose.length - 1; i++){
+            for (int j = 0; j <= jose.length -1; j++){
+
+                jose[i][j] = entrada.nextInt();
+
+
+            }
+        }
+    }
+}
